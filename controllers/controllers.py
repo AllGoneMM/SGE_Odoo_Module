b86014186ee0adff4e7cfab2dccca12c0ada6649
Mykyta Metelytsya <shadyrnb@gmail.com>
2023-01-16 18:00:46 +0100

    first commit

---
+++ controllers/controllers.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# from odoo import http
+
+
+# class C:\users\shady\desktop\(http.Controller):
+#     @http.route('/c:\users\shady\desktop\/c:\users\shady\desktop\/', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world"
+
+#     @http.route('/c:\users\shady\desktop\/c:\users\shady\desktop\/objects/', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('c:\users\shady\desktop\.listing', {
+#             'root': '/c:\users\shady\desktop\/c:\users\shady\desktop\',
+#             'objects': http.request.env['c:\users\shady\desktop\.c:\users\shady\desktop\'].search([]),
+#         })
+
+#     @http.route('/c:\users\shady\desktop\/c:\users\shady\desktop\/objects/<model("c:\users\shady\desktop\.c:\users\shady\desktop\"):obj>/', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('c:\users\shady\desktop\.object', {
+#             'object': obj
+#         })
---
+++ models/models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+
+# class c:\users\shady\desktop\(models.Model):
+#     _name = 'c:\users\shady\desktop\.c:\users\shady\desktop\'
+#     _description = 'c:\users\shady\desktop\.c:\users\shady\desktop\'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
