5677ef9fc54772249825eea866671d2001c711aa
Pablo Muñoz <699328@alu.murciaeduca.es>
2023-02-08 10:57:46 +0100

    Finalmente campos One2many implementados

--- models/project.py
+++ models/project.py
@@ -25,16 +25,16 @@ class EnterpriseProject(models.Model):
     ],string="Status",default='draft',tracking = True)
     
     worker_ids = fields.One2many('person.worker','name',string = 'Trabajadores asignados',tracking = True)
-    worker_count =fields.Integer(string='Número de empleados', compute='get_workers_count',tracking = True)
+    # worker_count =fields.Integer(string='Número de empleados', compute='get_workers_count',tracking = True)
     
-
+    project_lines = fields.One2many('enterprise.project.lines','project_id',string="Project Lines")
 
     
     #Métodos
 
-    def get_workers_count(self):
-        count = self.env['person.worker'].search_count([('project_id', '=', self.id)])
-        self.worker_count = count
+    # def get_workers_count(self):
+    #     count = self.env['person.worker'].search_count([('project_id', '=', self.id)])
+    #     self.worker_count = count
 
     # Método que cambia la propiedad state para el botón del header
     def action_confirm(self):
@@ -51,3 +51,11 @@ class EnterpriseProject(models.Model):
     # Método que cambia la propiedad state para el botón del header
     def action_cancel(self):
         self.state = 'cancel'
+
+class EnterpriseProjectLines(models.Model):
+    _name="enterprise.project.lines"
+    _description="Project Lines"
+
+    worker_id = fields.Many2one('person.worker',string ='Trabajador/a asignado')
+    worker_role =fields.Many2one('person.worker','role',string='Puesto de trabajo')
+    project_id = fields.Many2one('enterprise.project', string ='Id Proyecto')
